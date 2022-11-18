b14c8bc4ab8ac684cc7cbe44f22807161a361e71
owenshi2 <owenshi2@illinois.edu>
2022-11-18 17:39:28 -0600

    CSV parsing

--- src/parse.py
+++ src/parse.py
@@ -1,10 +1,18 @@
-# import pandas
+import pandas as pd
+import 
 
 f = open('./entry/routes.csv')
-
 
 airport = []
 
 for i in f:
   j = i.split(',')[2]
+  airport.append(j)
+
+airset = set(airport)
+# print(airset)
+
+f = open('./entry/airport-codes.csv')
+for i in f:
+  j = i.split(',')[4]
   print(j)
