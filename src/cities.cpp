607e8fbd40957afa23f1c0023ed1b461252e763b
OS <owenshi2@illinois.edu>
2022-11-22 16:20:03 -0600

    Parsing first method

--- src/cities.cpp
+++ src/cities.cpp
@@ -8,22 +8,4 @@
 #include <iterator>
 #include <algorithm>
 
-using std::string;
-using std::vector;
-using std::ifstream;
-using std::cout;
-using std::endl;
-using std::feof;
-
-ifstream words("worldcities.csv");
-vector<string> out; 
-
-// if (words.is_open()) {   
-//     std::istream_iterator<string> word_iter(words);
-//     while (!words.eof()) {
-//         out.push_back(remove_punct(*word_iter));
-//         ++word_iter;  
-//     }
-// }
-
 
--- src/parser.cpp
+++ src/parser.cpp
@@ -7,16 +7,17 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <sstream>
 
 using std::string;
-using std::vector;
+// using std::vector;
 using std::ifstream;
 using std::cout;
 using std::endl;
 using std::feof;
 
 ifstream words("worldcities.csv");
-vector<string> out; 
+std::vector<string> out; 
 
 // if (words.is_open()) {   
 //     std::istream_iterator<string> word_iter(words);
@@ -25,3 +26,25 @@ vector<string> out;
 //         ++word_iter;  
 //     }
 // } 
+
+std::vector<std::string> getEmAndSplitEm(std::istream& str)
+{
+    std::vector<std::string>   result;
+    std::string                line;
+    std::getline(str,line);
+
+    std::stringstream lineStream(line);
+    std::string cell;
+
+    while(std::getline(lineStream,cell, ','))
+    {
+        result.push_back(cell);
+    }
+    // This checks for a trailing comma with no data after it.
+    if (!lineStream && cell.empty())
+    {
+        // If there was a trailing comma then add an empty element.
+        result.push_back("");
+    }
+    return result;
+}
