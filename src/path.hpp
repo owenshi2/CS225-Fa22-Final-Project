9a13576157a5b3ba726316855b435d2585c1bbdf
OS <owenshi2@illinois.edu>
2022-12-10 22:37:57 -0600

    Basic Hierholzer Layout Established

--- src/path.hpp
+++ src/path.hpp
@@ -16,17 +16,36 @@ class Circuit
       {
         edgecount.insert({start.at(i).name, start.at(i).c_cities.size()});
       }
-      std::stack<std::string> curr;
-      std::vector<std::string> circuit;
-      curr.push(start.at(0).name);
-      std::string curr_ver = start.at(0).name;
+      std::stack<City> curr;
+      std::vector<City> circuit;
+      curr.push(start.at(0));
+      City curr_ver = start.at(0);
       while(!curr.empty())
       {
-        if(edgecount.at(curr_ver) > 0)
+        if(edgecount.at(curr_ver.name) > 0)
         {
           auto it = std::find(start.begin(), start.end(), curr_ver);
-          (*it).name;
-          curr.push(start.at(curr_ver))
+          curr.push(*(*it).c_cities.at((*it).c_cities.size()-1));
+          auto it2 = edgecount.find(curr_ver.name);
+          if(it2 != edgecount.end())
+          {
+            it2->second -= 1;
+          }
+          curr_ver = curr.top();
+        } else
+        {
+          circuit.push_back(curr.top());
+          curr_ver = curr.top();
+          curr.pop();
+        }
+      }
+      for(unsigned i = circuit.size()-1; i >= 0; i--)
+      {
+        //System.out.println();
+        std::cout << circuit.at(i).name << std::endl;
+        if(i != 0)
+        {
+          std::cout << " -> ";
         }
       }
     }
