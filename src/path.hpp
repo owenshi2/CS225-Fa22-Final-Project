cc901e04a0e2e5431181978076f2a7a5650c9e39
OS <owenshi2@illinois.edu>
2022-12-11 23:14:41 -0600

    Umm..ackshually...you don't push to main. :nerd face: - Sai

--- src/path.hpp
+++ src/path.hpp
@@ -9,28 +9,23 @@
 class Circuit
 {
   public:
-    void findCircuit(std::vector<City>& start)
+    void findCircuit(std::map<std::string, City> start)
     {
       std::map<std::string, int> edgecount;
-      for (int i = 0; i < start.size(); i++)
+      for (auto const& x : start)
       {
-        edgecount.insert({start.at(i).name, start.at(i).c_cities.size()});
+        edgecount.insert({x.first, x.second.c_cities.size()});
       }
       std::stack<City> curr;
       std::vector<City> circuit;
-      curr.push(start.at(0));
-      City curr_ver = start.at(0);
+      curr.push((*start.begin()).second);
+      City curr_ver = (*start.begin()).second;
       while(!curr.empty())
       {
-        if(edgecount.at(curr_ver.name) > 0)
+        if(edgecount.at(curr_ver.name))
         {
-          std::vector<City>::iterator it = std::find(start.begin(), start.end(), curr_ver);
-          curr.push(*(*it).c_cities.at((*it).c_cities.size()-1));
-          std::map<std::string, int>::iterator it2 = edgecount.find(curr_ver.name);
-          if(it2 != edgecount.end())
-          {
-            it2->second -= 1;
-          }
+          curr.push(*(start[curr_ver.name]).c_cities.at(start[curr_ver.name].c_cities.size() - 1));
+          edgecount[curr_ver.name]--;
           curr_ver = curr.top();
         } else
         {
@@ -39,11 +34,10 @@ class Circuit
           curr.pop();
         }
       }
-      for(unsigned i = circuit.size()-1; i >= 0; i--)
+      for(unsigned i = 0; i < circuit.size(); i++)
       {
-        //System.out.println();
-        std::cout << circuit.at(i).name << std::endl;
-        if(i != 0)
+        std::cout << circuit.at(i).name;
+        if(i != circuit.size() - 1)
         {
           std::cout << " -> ";
         }
